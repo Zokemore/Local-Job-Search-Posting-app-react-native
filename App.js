941a26f61d042b680ad43545f66554dd8d443ec1
Reebal <zetatechcc@gmail.com>
2022-02-13 18:20:54 +0530

    Day 1

--- App.js
+++ App.js
@@ -1,16 +1,17 @@
 
 import React from 'react';
 import {Text, View} from 'react-native';
-import {Header} from './Header';
-import {heading} from './Typography';
+//import {Header} from './Header';
+//import {heading} from './Typography';
 
-const WelcomeScreen = () => (
+const WelcomeScreen = () => {
+return (
   <View>
-    <Header title="Job Search"/>
-    <Text style={heading}>Step One</Text>
     <Text>
       Free & Open Source Job Posting App to fit local requirement!
     </Text>
    </View>
-);
+)
+};
   
+export default WelcomeScreen;
